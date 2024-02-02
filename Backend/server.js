20eacb7b549063db90739e4a0c7cd74d1c671ae7
Kethankrk <kethankrk@gmail.com>
2024-02-02 11:12:03 +0530

    mongodb config

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,16 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose
+  .connect(
+    "mongodb+srv://lolan:lolans12k@cluster0.ytaftoz.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to mongodb"))
+  .catch((error) => console.log(`error connecting to mongodb: ${error}`));
+
+db = mongoose.connection;
 app.get("/", (req, res) => {
-  res.send({"message": "hello world"});
+  res.send({ message: "hello world" });
 });
 
-
-app.listen(3000, () => console.log("Running server on http://localhost:3000"))
+app.listen(3000, () => console.log("Running server on http://localhost:3000"));
