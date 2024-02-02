ee033686149adb5bd2486d85bcbeea59794ff8ef
Kethankrk <kethankrk@gmail.com>
2024-02-02 18:52:22 +0530

    added readme and something

--- Client/src/Pages/LocalGuide/TravellerReqest.jsx
+++ Client/src/Pages/LocalGuide/TravellerReqest.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const TravellerReqest = () => {
-<<<<<<< HEAD
     const [request , setaddressRequest ] =useState("")
     const [email,setemail] = useState('')
     useEffect(() => {
@@ -38,56 +37,5 @@ const TravellerReqest = () => {
                     </div> </>
     );
 }
-=======
-  const [request, setaddressRequest] = useState("");
-  const [email, setemail] = useState("");
-  useEffect(() => {
-    setemail(localStorage.getItem("email"));
-    axios.post("http://localhost:3000/guide/get-req", { email }).then((res) => {
-      console.log(res.data.data);
-    });
-  }, []);
-  return (
-    <>
-      <h1
-        className="font-bold text-xl
-                    "
-      >
-        Traveller Requests
-      </h1>
-      <div className="flex flex-col gap-5 mt-3">
-        <div className="flex justify-between items-center bg-gray-100 px-3 py-3 rounded-xl shadow-md">
-          <div className="flex gap-3">
-            <img
-              src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
-              alt=""
-              className="h-20 rounded-full"
-            />
-            <div className="flex flex-col ">
-              <h1 className="font-semibold text-xl">Name</h1>
-              <h1>place</h1>
-            </div>
-          </div>
-          <h1 className="font-bold">Destination:</h1>
-          <div className="flex gap-3">
-            <button
-              type="button"
-              className="px-8 py-1 font-semibold rounded-full bg-[#6C63FF] shadow-md text-gray-100"
-            >
-              Accept
-            </button>
-            <button
-              type="button"
-              className="px-8 py-1 font-semibold rounded-full bg-[#6C63FF] shadow-md text-gray-100"
-            >
-              Decline
-            </button>
-          </div>
-        </div>
-      </div>{" "}
-    </>
-  );
-};
->>>>>>> 7b91831a24c14f63720c5eb7b55de5842f39c2d9
 
 export default TravellerReqest;
