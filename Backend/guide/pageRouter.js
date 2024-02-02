0422ecb5f8c70e0fb959d61e9f67d6443192e8bc
hibhatharagma09@gmail.com <157917395+HamiYasir@users.noreply.github.com>
2024-02-02 13:15:47 +0530

    Update pageRouter.js

--- Backend/guide/pageRouter.js
+++ Backend/guide/pageRouter.js
@@ -1,11 +1,11 @@
 const router=require('express').Router()
-const Database=require('./models/Model')
+const {Guide, Client} =require('../models/Model')
 
 router.post('/signup',async(req,res)=>{
     console.log('signup page reached[POST]')
-    const Database_signup=await Database.findOne({email:req.body.email})
-    if(!Database_signup){
-        await Database.insertMany([req.body])
+    const Guide_signup=await Guide.findOne({email:req.body.email})
+    if(!Guide_signup){
+        await Guide.insertMany([req.body])
         res.json({status:true})
     }else{
         res.json({status:false})
@@ -14,13 +14,24 @@ router.post('/signup',async(req,res)=>{
 
 router.post('/login',async(req,res)=>{
     console.log('login page reached[POST]')
-    const Database_login=await Database.findOne({email:req.body.email})
+    const Guide_login=await Guide.findOne({email:req.body.email})
     if(login==null){
         res.json({status:false})
-    }else if(req.body.password===Database_login.password){
+    }else if(req.body.password===Guide_login.password){
         res.json({status:true})
     }else{
         res.json({status:false})
+    }
+})
+
+router.post('/setVehicle', async(req, res)=>{
+    console.log("setVehicle page reached[POST")
+    const {email, vehicles}=req.body
+    const Guide_setVehicle=await Guide.findOne({email})
+    if(Guide_setVehicle){
+        await Guide.insertMany({vehicles})
+    }else{
+        throw new Error('No guide found with given email')
     }
 })
 
