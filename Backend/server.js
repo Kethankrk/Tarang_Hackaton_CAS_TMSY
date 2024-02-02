04a27ea3d490497591a9a3e1449d2fa7c812a509
Kethankrk <kethankrk@gmail.com>
2024-02-02 12:18:14 +0530

    modifed model and created signup route

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const { Client, Guide } = require("./models/Model");
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 
 mongoose
   .connect(
@@ -17,37 +21,50 @@ app.get("/", (req, res) => {
 
 app.post("/client/singup", async (req, res) => {
   try {
-    const { email, password, userImage, address, phone, idProof } = req.data;
-    if (!email || !password || !userImage || !address || !phone || !idProof) {
+    console.log(req.body)
+    const { email, name, password, userImage, address, phone, idProof } =
+      req.body;
+    if (
+      !email ||
+      !password ||
+      !userImage ||
+      !address ||
+      !phone ||
+      !idProof ||
+      !name
+    ) {
       return res.status(400).send({
         status: "failure",
         error: "Some fields are empty",
       });
+    }
 
-      existingUser = await client.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({
-          status: "failure",
-          error: "User with this email already exists.",
-        });
-      }
-      let newClient = await new client({
-        email,
-        password,
-        userImage,
-        address,
-        phone,
-        idProof,
-      });
-
-      await newClient.save();
-      return res.status(200).json({
-        status: "success",
+    let existingUser = await Client.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        status: "failure",
+        error: "User with this email already exists.",
       });
     }
+    let newClient = new Client({
+      email,
+      name,
+      password,
+      userImage,
+      address,
+      phone,
+      idProof,
+    });
+
+    await newClient.save();
+    return res.status(200).json({
+      status: "success",
+    });
   } catch (e) {
+    console.log(e)
     return res.status(500).json({ error: e.name });
   }
 });
+
 
 app.listen(3000, () => console.log("Running server on http://localhost:3000"));
