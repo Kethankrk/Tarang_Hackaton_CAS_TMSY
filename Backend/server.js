29c26d78b899c6d65eb18403ada3dd8d38f46ec8
Kethankrk <kethankrk@gmail.com>
2024-02-02 11:43:03 +0530

    client login route

--- Backend/server.js
+++ Backend/server.js
@@ -15,4 +15,39 @@ app.get("/", (req, res) => {
   res.send({ message: "hello world" });
 });
 
+app.post("/client/singup", async (req, res) => {
+  try {
+    const { email, password, userImage, address, phone, idProof } = req.data;
+    if (!email || !password || !userImage || !address || !phone || !idProof) {
+      return res.status(400).send({
+        status: "failure",
+        error: "Some fields are empty",
+      });
+
+      existingUser = await client.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          status: "failure",
+          error: "User with this email already exists.",
+        });
+      }
+      let newClient = await new client({
+        email,
+        password,
+        userImage,
+        address,
+        phone,
+        idProof,
+      });
+
+      await newClient.save();
+      return res.status(200).json({
+        status: "success",
+      });
+    }
+  } catch (e) {
+    return res.status(500).json({ error: e.name });
+  }
+});
+
 app.listen(3000, () => console.log("Running server on http://localhost:3000"));
