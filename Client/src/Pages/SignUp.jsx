29bce61cf8c1c3486a97a58281c5a9dee6ebc672
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-02-02 12:52:22 +0530

    Signup page

--- Client/src/Pages/SignUp.jsx
+++ Client/src/Pages/SignUp.jsx
@@ -1,7 +1,17 @@
 import { IKContext, IKUpload } from 'imagekitio-react';
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+
+   const [name ,Setname] = useState("");
+   const [email, setemail] = useState("");
+   const [number, SetNumber] = useState("");
+   const [address, setaddress] = useState("");
+   const [idproff,SetIdProof] = useState("");
+   const [password, setPass] = useState("");
+   const [passconfo, Setpassconfo] = useState("");
+
+
    
     return (
         <div className='flex w-full h-screen bg-[#6C63FF]  items-center px-48 justify-center'>
@@ -16,11 +26,18 @@ const SignUp = () => {
             <div className="space-y-4">
 			<div>
 				<label for="Name" className="block mb-2 text-sm">Name</label>
-				<input type="text" name="Name" id="Name" placeholder="leroy****" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="text" name="Name" id="Name" placeholder="leroy****" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                required
+                value={name}
+                onChange={e=> Setname(e.target.value)}
+                />
 			</div>
 			<div>
 				<label for="email" className="block mb-2 text-sm">Email address</label>
-				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                required
+                value={email}
+                onChange={e=> setemail(e.target.value)}/>
 			</div>
             
 
@@ -30,41 +47,64 @@ const SignUp = () => {
             <div className="space-y-4">
 			<div>
 				<label for="number" className="block mb-2 text-sm">Phone Number</label>
-				<input type="number" name="number" id="number" placeholder="+91 **********" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="number" name="number" id="number" placeholder="+91 **********" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                value={number}
+                onChange={e=> SetNumber(e.target.value)}
+                required
+                />
 			</div>
 			<div>
 				<label for="Address" className="block mb-2 text-sm">Address</label>
-				<input type="text" name="Address" id="Address" placeholder="************" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="text" name="Address" id="Address" placeholder="************" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                required
+                value={address}
+                onChange={e=> setaddress(e.target.value)}/>
 			</div>
 		</div>
             </div>
         </div>
         <div>
 				<label for="doc" className="block mb-2 text-sm">Id Proof</label>
-				<input type="file" name="doc" id="doc" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="file" name="doc" id="doc" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                required/>
 			</div>
-		<div className="flex  gap-3">
+		<div className="flex gap-3">
             <div>
 				<div className="flex justify-between mb-2">
 					<label for="password" className="text-sm">Password</label>
 				</div>
-				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                 required
+                 value={password}
+             onChange={e=>setPass(e.target.value)}/>
 			</div>
             <div>
 				<div className="flex justify-between mb-2">
 					<label for="password" className="text-sm">Password</label>
+					<a rel="noopener noreferrer" href="#" className="text-xs hover:underline text-gray-600">
+  {passconfo === password ? (
+    <p></p>
+  ) : (
+    <p>Passwords do not match</p>
+  )}
+</a>
 				</div>
-				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" />
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md border-gray-300 bg-gray-50 text-gray-800" 
+                required 
+                value={passconfo}
+                onChange={e=> Setpassconfo(e.target.value)}/>
 			</div>
+
             </div>
+            
 
 		<div className="space-y-2">
             
 			<div>
 				<button type="sumbit" className="w-full px-8 py-3 font-semibold rounded-md bg-violet-600 text-gray-50">Sign up</button>
 			</div>
-			<p className="px-6 text-sm text-center text-gray-600">Don't have an account yet?
-				<a rel="noopener noreferrer" href="#" className="hover:underline text-violet-600">Sign up</a>.
+			<p className="px-6 text-sm text-center text-gray-600">Do you have an account already?
+				<a rel="noopener noreferrer" href="#" className="hover:underline text-violet-600">Sign in</a>.
 			</p>
 		</div>
 	</form>
