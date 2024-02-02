5a25c3b5777c6751df600bd0ea9827e603559c7c
Kethankrk <kethankrk@gmail.com>
2024-02-02 13:00:02 +0530

    created get-guide route

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const { Client, Guide } = require("./models/Model");
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 mongoose
   .connect(
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 
 app.post("/client/singup", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const { email, name, password, userImage, address, phone, idProof } =
       req.body;
     if (
@@ -46,6 +46,7 @@ app.post("/client/singup", async (req, res) => {
         error: "User with this email already exists.",
       });
     }
+    address = address.trim().toLowerCase();
     let newClient = new Client({
       email,
       name,
@@ -61,10 +62,67 @@ app.post("/client/singup", async (req, res) => {
       status: "success",
     });
   } catch (e) {
-    console.log(e)
+    console.log(e);
     return res.status(500).json({ error: e.name });
   }
 });
 
+app.post("/client/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).send({
+        status: "failure",
+        error: "Some fields are empty",
+      });
+    }
+
+    const user = await Client.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        status: "failure",
+        error: "User Not found",
+      });
+    }
+
+    if (password != user.password) {
+      return res.status(400).json({
+        status: "failure",
+        error: "Incorrect password",
+      });
+    }
+
+    return res.status(200).json({
+      status: "success",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: e.name });
+  }
+});
+
+app.post("/client/get-guide", async (req, res) => {
+  try {
+    let { address } = req.body;
+    if (!address) {
+      return res.status(400).json({
+        status: "failure",
+        error: "address is missing",
+      });
+    }
+    address = address.trim().toLowerCase();
+
+    const guides = await Guide.find({ address });
+
+    return res.status(200).json({
+      status: "success",
+      guides,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: e.name });
+  }
+});
 
 app.listen(3000, () => console.log("Running server on http://localhost:3000"));
